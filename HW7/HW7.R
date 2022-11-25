383a99135cdfd8a15000a87141c7f97d2af56b40
connordouglashanna <connordouglashanna@gmail.com>
2022-11-25 01:42:20 -0600

    Update HW7.R
    Added some additional code. Most of the homework was completed using the console but I'm saving this anyway.

--- HW7/HW7.R
+++ HW7/HW7.R
@@ -5,5 +5,13 @@ large_n_zscore <- function(x, y, s, d, n, m) {
   print(z)
 }
 
+# 10.8 a)
+
+pbinom(4, 15, .1, lower.tail = FALSE) + 
+  pbinom(3, 15, .1, lower.tail = F)*dbinom(3, 15, .1) +
+  pbinom(4, 15, .1, lower.tail = F)*dbinom(2, 15, .1) +
+  pbinom(5, 15, .1, lower.tail = F)*dbinom(1, 15, .1) +
+  pbinom(6, 15, .1, lower.tail = F)*dbinom(0, 15, .1)
+
 # 10.17 c)
-large_n_zscore(9017, 5853, 7162,1961, 130, 80)
+large_n_zscore(9017, 5853, 7162, 1961, 130, 80)
